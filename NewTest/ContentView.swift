a4a62f201f96fcae1839164c37645c0902824cab
Curtis Scott <curtis.scott@gocity.com>
2023-05-03 22:26:09 +0100

    ok

--- NewTest/ContentView.swift
+++ NewTest/ContentView.swift
@@ -16,7 +16,6 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hello, world!")
             Text("Hello, world!")
-            Text("Hello, world!")
         }
         .padding()
     }
