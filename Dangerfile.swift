cf86093a8e9012c28b70f85956217688d2dfb056
Curtis Scott <curtis.scott@gocity.com>
2023-05-04 01:13:45 +0100

    test

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,3 +2,11 @@ import Danger
 let danger = Danger()
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
+
+let changelogChanged = editedFiles.contains("CHANGELOG.md")
+
+if !changelogChanged  {
+  warn("No CHANGELOG entry added.")
+} else {
+    warn(" CHANGELOG entry added.")
+}
